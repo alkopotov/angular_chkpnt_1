d64d04adab49f38577b3a22cbe544f73fdc52a90
AlHobb <118611357+alkopotov@users.noreply.github.com>
2024-03-22 16:30:08 +0300

    Ver 1.1.0

--- src/app/service/product-list.service.ts
+++ src/app/service/product-list.service.ts
@@ -18,24 +18,18 @@ export class ProductListService {
 
   public categorySelected: string = 'all';
 
-  public priceRangeMin: number = this.minPrice;
-  
-  public priceRangeMax: number = this.maxPrice;
-
-  public priceRangeValue: number = Math.floor((this.minPrice + this.maxPrice) / 2);
 
   public fetchProducts(): void {
     fetch('https://dummyjson.com/products?limit=50')
       .then(res => res.json())
         .then(data =>  {
-          this.products = data.products
-          this.definePriceInput();
+          this.products = data.products;
         })
   }
 
 
   private get _initialPriceList(): number[] {
-    return this.products.map((elem: any) => +elem.price).sort((a: number, b: number) => a - b)
+    return this._filteredProducts.map((elem: any) => +elem.price).sort((a: number, b: number) => a - b)
   }
 
   public get maxPrice(): number {
@@ -46,25 +40,32 @@ export class ProductListService {
     return this._initialPriceList[0];
   }
 
+  
   private get _displayedPriceList(): number[] {
     return this.displayedProducts.map((elem: any) => +elem.price).sort((a: number, b: number) => a - b)
   }
 
   public get defaultValue(): number {
-    if (this.currentPrice === '') return this.maxPrice
-    if (+this.currentPrice < this.maxPrice) {
-      return +this.currentPrice
-    }
-    return this.maxPrice
+    if (this.currentPrice === '') return Math.floor((this.maxPrice + this.minPrice) / 2)
+    
+    return Math.floor((this.maxPrice + this.minPrice) / 2)
   }
 
+
+  private get _filteredProducts(): any[] {
+    return this.products.filter(product => {
+      return product.title.toLowerCase().includes(this.searchValue.toLocaleLowerCase())
+        && (this.categorySelected === 'all' || product.category === this.categorySelected)
+        && (Math.round(product.rating) <= +this.maxRating)
+    });
+
+  }
   public get displayedProducts() : any[] {
     return this.products.filter(product => {
       return product.title.toLowerCase().includes(this.searchValue.toLocaleLowerCase())
         && (this.categorySelected === 'all' || product.category === this.categorySelected)
         && (Math.round(product.rating) <= +this.maxRating)
-        && (this.currentPrice === '' || product.price <= this.currentPrice)
-
+        && (this.currentPrice === '' || product.price <= +this.currentPrice)
     });
   }
 
@@ -75,9 +76,7 @@ export class ProductListService {
     return [...new Set(this.displayedProducts.map((elem: any) => elem.category)), 'all'].sort();
   }
 
-  public definePriceInput(): void {
-    this.priceRangeMin = this._displayedPriceList[0];
-    this.priceRangeMax = this._displayedPriceList[this._displayedPriceList.length - 1];
-    this.priceRangeValue = Math.floor((this.priceRangeMin + this.priceRangeMax) / 2)
+  public resetPriceInput(): void {
+    this.currentPrice = '';
   }
 }
